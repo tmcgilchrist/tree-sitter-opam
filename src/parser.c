1584f51b10a74dbc26fa5fa38dc095d0dcd8bd0a
Tim McGilchrist <timmcgil@gmail.com>
2026-03-27 17:27:36 +1100

    Use ABI 14
    This should be more compatible with Emacs 29/30

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 102
 #define LARGE_STATE_COUNT 29
 #define SYMBOL_COUNT 67
@@ -1349,7 +1349,7 @@ static bool ts_lex_keywords(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 24},
   [2] = {.lex_state = 0},
@@ -4486,7 +4486,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_opam(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -4506,13 +4505,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_opam(void) {
     .keyword_lex_fn = ts_lex_keywords,
     .keyword_capture_token = sym_ident,
     .primary_state_ids = ts_primary_state_ids,
-    .name = "opam",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 0,
-    },
   };
   return &language;
 }
